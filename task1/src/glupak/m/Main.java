a971246e221213678a70beab960d8c90db8cd0eb
Glupak <Glupak@GLUPAK>
2017-11-14 10:51:52 +1000

    Task1 complete

--- task1/src/glupak/m/Main.java
+++ task1/src/glupak/m/Main.java
@@ -1,13 +1,32 @@
 package glupak.m;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
 
+    public static void main(String[] args) {
+        // write your code here
+        for (int value : Solution(new int[]{2, 7, 11, 15}, 9)) {
+            System.out.println(value);
+        }
     }
 
-    public int[] Solution(int[] nums,int arg){
-        return null;
+    private static int[] Solution(int[] nums, int arg) {
+        int[] result = new int[2];
+        boolean flag = false;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (flag) {
+                break;
+            }
+            result[0] = i;
+            for (int y = i + 1; y < nums.length; y++) {
+                if (nums[i] + nums[y] == arg) {
+                    result[1] = y;
+                    flag=true;
+                }
+            }
+        }
+        return result;
     }
 }
